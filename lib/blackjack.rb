4dfe8270edee283dfe4a040ed24bd1bcfad955c4
Ekene N <ekenayy@gmail.com>
2020-10-26 00:20:00 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -67,7 +67,7 @@ def runner
   first_round_number = initial_round 
  if hit?(first_round_number) === first_round_number
    hit?(first_round_number)
- elsif hit?(first_round_number) > first_round_number
+ elsif hit?(first_round_number) > first_round_number && <
   binding.pry
 #if card_total > 21
   #end_game 
